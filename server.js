0bc2d8347f9b9cd7890c213c0f597d550fa71b2e
Devipriya2305 <devipriya0523@gmail.com>
2025-12-16 20:42:50 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from 'express'
+import userRouter from './routes/user.js'
+import recipeRouter from './routes/recipe.js'
+import cors from 'cors'
+const app = express(); 
+
+app.use(bodyParser.json())
+app.use(cors({
+  origin:true,
+  methods:["GET","POST","PUT","DELETE"],
+  credentials:true
+ 
+}))
+
+// userRouter
+app.use('/api',userRouter)
+
+// recipeRouter
+app.use('/api',recipeRouter)
+
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/recipe_sharing", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB is Connected..!"))
+  .catch((err) => console.log(err.message));
+
+const port = 3000;
+app.listen(port, () => console.log(`server is running on port ${port}`));
